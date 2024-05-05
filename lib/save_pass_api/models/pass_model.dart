ff17e81bd014788a70946b238795f26127391b59
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-05 21:26:43 +0300

    Update pass_model.dart

--- lib/save_pass_api/models/pass_model.dart
+++ lib/save_pass_api/models/pass_model.dart
@@ -1,8 +1,18 @@
 class PassModel {
   String passwordName;
   String password;
+  int passwordId;
   PassModel({
     required this.passwordName,
     required this.password,
+    required this.passwordId,
   });
+
+  Map<String, Object?> toMap() {
+    return {
+      'password': password,
+      'password_name': passwordName,
+      'password_id': passwordId
+    };
+  }
 }
