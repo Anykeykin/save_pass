60faac9d709c5cbb4767df048cd19238d61e54a1
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-05 22:44:46 +0300

    add methods on abstract repository

--- lib/save_pass_api/remote_api/repository/remote_repository.dart
+++ lib/save_pass_api/remote_api/repository/remote_repository.dart
@@ -1,7 +1,23 @@
+import 'package:save_pass/save_pass_api/models/pass_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 abstract class RemoteRepository {
-  void savePass();
-  readPass();
-  getAllPass();
-  editPass();
-  deletePass();
+  Future<bool> savePass(
+    SupabaseClient supabase,
+    PassModel passModel,
+  );
+
+  Future<List<PassModel>> getAllPass(
+    SupabaseClient supabase,
+  );
+
+  Future<bool> editPass(
+    SupabaseClient supabase,
+    PassModel passModel,
+  );
+
+  Future<bool> deletePass(
+    SupabaseClient supabase,
+    int passwordId,
+  );
 }
