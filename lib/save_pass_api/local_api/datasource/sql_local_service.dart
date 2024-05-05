ec63b66257e5cb0f43c9746256a84b8db1f31225
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-05 20:44:21 +0300

    add start return

--- lib/save_pass_api/local_api/datasource/sql_local_service.dart
+++ lib/save_pass_api/local_api/datasource/sql_local_service.dart
@@ -1,1 +1,23 @@
-class SqlLocalService {}
+import 'package:save_pass/save_pass_api/models/pass_model.dart';
+
+class SqlLocalService {
+  static bool deletePass() {
+    return true;
+  }
+
+  static bool editPass() {
+    return true;
+  }
+
+  static bool savePass(PassModel passModel) {
+    return true;
+  }
+
+  static List<PassModel> getAllPass() {
+    return [];
+  }
+
+  static PassModel readPass() {
+    return PassModel(passwordName: 'passwordName', password: 'password');
+  }
+}
