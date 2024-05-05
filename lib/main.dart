684498326912b3372edc3877ae2a38fb1d5de09f
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-05 22:21:27 +0300

    upd

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:save_pass/save_pass_api/remote_api/datasource/initialize.dart';
 import 'package:save_pass/save_pass_ui/first_screen.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  initialize();
   runApp(const MyApp());
 }
