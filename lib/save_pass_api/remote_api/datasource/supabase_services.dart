23a2a4fc86f6176c0d73e8ac0d9d50fd19ec97f9
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-05 22:46:03 +0300

    add pass methods realization on supabase service

--- lib/save_pass_api/remote_api/datasource/supabase_services.dart
+++ lib/save_pass_api/remote_api/datasource/supabase_services.dart
@@ -1,1 +1,33 @@
-class SupabaseService {}
+import 'package:save_pass/save_pass_api/models/pass_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseAppService {
+  static deletePass(SupabaseClient supabase, int passwordId) async {
+    dynamic test =
+        await supabase.from('pass').delete().match({'password_id': passwordId});
+    print(test);
+    return true;
+  }
+
+  static editPass(SupabaseClient supabase, PassModel passModel) async {
+    dynamic test = await supabase
+        .from('pass')
+        .update(passModel.toMap())
+        .match({'password_id': passModel.passwordId});
+    print(test);
+    return true;
+  }
+
+  static getAllPass(SupabaseClient supabase) async {
+    final data = await supabase.from('users').select();
+    return [
+      for (final passMap in data) PassModel.fromMap(passMap),
+    ];
+  }
+
+  static savePass(SupabaseClient supabase, PassModel passModel) async {
+    dynamic test = await supabase.from('cities').insert(passModel.toMap());
+    print(test);
+    return true;
+  }
+}
