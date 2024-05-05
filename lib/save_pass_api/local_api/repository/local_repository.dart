47d263bfcba2dc2fb2a7017f25bf20856086a2d7
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-05 20:23:26 +0300

    create pattern local api

---
+++ lib/save_pass_api/local_api/repository/local_repository.dart
@@ -0,0 +1,7 @@
+abstract class LocalRepository {
+  void savePass();
+  readPass();
+  getAllPass();
+  editPass();
+  deletePass();
+}
---
+++ lib/save_pass_api/local_api/repository/local_repository_impl.dart
@@ -0,0 +1,32 @@
+import 'package:save_pass/save_pass_api/local_api/repository/local_repository.dart';
+
+class LocalRepositoryImpl implements LocalRepository {
+  @override
+  deletePass() {
+    // TODO: implement deletePass
+    throw UnimplementedError();
+  }
+
+  @override
+  editPass() {
+    // TODO: implement editPass
+    throw UnimplementedError();
+  }
+
+  @override
+  getAllPass() {
+    // TODO: implement getAllPass
+    throw UnimplementedError();
+  }
+
+  @override
+  readPass() {
+    // TODO: implement readPass
+    throw UnimplementedError();
+  }
+
+  @override
+  void savePass() {
+    // TODO: implement savePass
+  }
+}
