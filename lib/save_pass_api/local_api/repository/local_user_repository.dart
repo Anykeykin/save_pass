c18ab547fadeac1277ff1e84d253d70d368c847e
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-07 08:44:56 +0300

    create local user repository

---
+++ lib/save_pass_api/local_api/repository/local_user_repository.dart
@@ -0,0 +1,4 @@
+abstract class LocalUserRepository {
+  saveAuthData();
+  loadAuthData();
+}
---
+++ lib/save_pass_api/local_api/repository_impl/local_user_repository_impl.dart
@@ -0,0 +1,15 @@
+import 'package:save_pass/save_pass_api/local_api/repository/local_user_repository.dart';
+
+class LocalUserRepositoryImpl implements LocalUserRepository {
+  @override
+  loadAuthData() {
+    // TODO: implement loadAuthData
+    throw UnimplementedError();
+  }
+
+  @override
+  saveAuthData() {
+    // TODO: implement saveAuthData
+    throw UnimplementedError();
+  }
+}
