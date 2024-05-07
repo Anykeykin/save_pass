776c4c94175c03c7b134f653336eb8206097817f
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-07 21:45:46 +0300

    add decode & encode methods

--- lib/save_pass_api/models/app_user.dart
+++ lib/save_pass_api/models/app_user.dart
@@ -8,4 +8,20 @@ class AppUser {
     required this.password,
     required this.uid,
   });
+
+  Map<String, Object?> toMap() {
+    return {
+      'password': password,
+      'email': email,
+      'uid': uid,
+    };
+  }
+
+  static AppUser fromMap(Map<String, dynamic> map) {
+    return AppUser(
+      email: map['email'],
+      password: map['password'],
+      uid: 'uid',
+    );
+  }
 }
