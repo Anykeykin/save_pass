b9e1b088176c4d03931e98f88a8c3431fd5f6853
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-07 21:46:20 +0300

    rewrite auth methods on repository

--- lib/save_pass_api/local_api/repository/local_user_repository.dart
+++ lib/save_pass_api/local_api/repository/local_user_repository.dart
@@ -1,4 +1,8 @@
+import 'package:save_pass/save_pass_api/models/app_user.dart';
+import 'package:sqflite/sqflite.dart';
+
 abstract class LocalUserRepository {
-  saveAuthData();
-  loadAuthData();
+  saveAuthData(AppUser appUser, Database database);
+  loadAuthData(Database database);
+  removeAuthData(AppUser appUser, Database database);
 }
--- lib/save_pass_api/local_api/repository_impl/local_user_repository_impl.dart
+++ lib/save_pass_api/local_api/repository_impl/local_user_repository_impl.dart
@@ -1,15 +1,21 @@
+import 'package:save_pass/save_pass_api/local_api/datasource/sql_local_service.dart';
 import 'package:save_pass/save_pass_api/local_api/repository/local_user_repository.dart';
+import 'package:save_pass/save_pass_api/models/app_user.dart';
+import 'package:sqflite/sqflite.dart';
 
 class LocalUserRepositoryImpl implements LocalUserRepository {
   @override
-  loadAuthData() {
-    // TODO: implement loadAuthData
-    throw UnimplementedError();
+  Future<AppUser> loadAuthData(Database database) async {
+    return await SqlLocalService.loadAuthData(database);
   }
 
   @override
-  saveAuthData() {
-    // TODO: implement saveAuthData
-    throw UnimplementedError();
+  Future<bool> saveAuthData(AppUser appUser, Database database) async {
+    return await SqlLocalService.saveAuthData(appUser, database);
+  }
+
+  @override
+  removeAuthData(AppUser appUser, Database database) async {
+    return await SqlLocalService.removeAuthData(appUser, database);
   }
 }
