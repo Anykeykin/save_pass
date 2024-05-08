57d0b854602c479fe1533b4499693b3901d8684b
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-08 22:07:59 +0300

    update abstract class userRepository

--- lib/save_pass_api/remote_api/repository/user_repository.dart
+++ lib/save_pass_api/remote_api/repository/user_repository.dart
@@ -1,5 +1,17 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 abstract class UserRepository {
-  void login();
-  void register();
-  void logout();
+  Future<User?> login(
+    SupabaseClient supabase,
+    String email,
+    String password,
+  );
+  Future<User?> register(
+    SupabaseClient supabase,
+    String email,
+    String password,
+  );
+  Future<void> logout(
+    SupabaseClient supabase,
+  );
 }
