eb0bfff9fb4911f74e264d8c6a458e87d2a801c6
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-08 22:58:34 +0300

    Update authorization_bloc.dart

--- lib/save_pass_bloc/authorization_bloc/authorization_bloc.dart
+++ lib/save_pass_bloc/authorization_bloc/authorization_bloc.dart
@@ -1,13 +1,21 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
 
 part 'authorization_event.dart';
 part 'authorization_state.dart';
 
 class AuthorizationBloc extends Bloc<AuthorizationEvent, AuthorizationState> {
-  AuthorizationBloc() : super(AuthorizationInitial()) {
-    on<AuthorizationEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  AuthorizationBloc() : super(AuthorizationState()) {
+    on<Login>(_login);
+    on<Logout>(_logout);
+    on<Register>(_register);
   }
+
+  FutureOr<void> _login(Login event, Emitter<AuthorizationState> emit) {}
+
+  FutureOr<void> _logout(Logout event, Emitter<AuthorizationState> emit) {}
+
+  FutureOr<void> _register(Register event, Emitter<AuthorizationState> emit) {}
 }
