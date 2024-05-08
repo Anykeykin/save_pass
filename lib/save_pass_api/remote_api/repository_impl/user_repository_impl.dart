dddc66f148ab73c26aecfee1e23de3b105520d46
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-08 22:08:28 +0300

    upd UserRepositoryImpl

--- lib/save_pass_api/remote_api/repository_impl/user_repository_impl.dart
+++ lib/save_pass_api/remote_api/repository_impl/user_repository_impl.dart
@@ -1,18 +1,37 @@
 import 'package:save_pass/save_pass_api/remote_api/repository/user_repository.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class UserRepositoryImpl implements UserRepository {
   @override
-  void login() {
-    // TODO: implement login
+  Future<User?> login(
+    SupabaseClient supabase,
+    String email,
+    String password,
+  ) async {
+    final authorization = await supabase.auth.signInWithPassword(
+      email: email,
+      password: password,
+    );
+    return authorization.user;
   }
 
   @override
-  void logout() {
-    // TODO: implement logout
+  Future<User?> register(
+    SupabaseClient supabase,
+    String email,
+    String password,
+  ) async {
+    final authorization = await supabase.auth.signUp(
+      email: email,
+      password: password,
+    );
+    return authorization.user;
   }
 
   @override
-  void register() {
-    // TODO: implement register
+  Future<void> logout(
+    SupabaseClient supabase,
+  ) async {
+    await supabase.auth.signOut();
   }
 }
