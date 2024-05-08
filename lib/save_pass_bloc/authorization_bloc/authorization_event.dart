b58ba810f0e5ce035ef02bd670900505c2a6f227
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-08 22:58:24 +0300

    create new events

--- lib/save_pass_bloc/authorization_bloc/authorization_event.dart
+++ lib/save_pass_bloc/authorization_bloc/authorization_event.dart
@@ -1,4 +1,30 @@
 part of 'authorization_bloc.dart';
 
-@immutable
-sealed class AuthorizationEvent {}
+abstract class AuthorizationEvent extends Equatable {
+  const AuthorizationEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class Login extends AuthorizationEvent {
+  final String email;
+  final String password;
+
+  const Login({required this.email, required this.password});
+
+  @override
+  List<Object> get props => [email, password];
+}
+
+class Logout extends AuthorizationEvent {}
+
+class Register extends AuthorizationEvent {
+  final String email;
+  final String password;
+
+  const Register({required this.email, required this.password});
+
+  @override
+  List<Object> get props => [email, password];
+}
