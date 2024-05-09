c92cfe9002a08793358ee65fdf33f38909099c23
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-10 01:09:59 +0300

    add events on bloc

--- lib/save_pass_bloc/passwords_bloc/passwords_bloc.dart
+++ lib/save_pass_bloc/passwords_bloc/passwords_bloc.dart
@@ -1,12 +1,29 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:save_pass/save_pass_api/models/pass_model.dart';
 
 part 'passwords_event.dart';
 part 'passwords_state.dart';
 
 class PasswordsBloc extends Bloc<PasswordsEvent, PasswordsState> {
-  PasswordsBloc() : super(PasswordsInitial()) {
-    on<PasswordsEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  PasswordsBloc() : super(const PasswordsState()) {
+    on<SavePass>(_savePass);
+    on<EditPass>(_editPass);
+    on<DeletePass>(_deletePass);
+    on<GetAllPass>(_getAllPass);
   }
+
+  FutureOr<void> _savePass(
+      SavePass event, Emitter<PasswordsState> emit) async {}
+
+  FutureOr<void> _editPass(
+      EditPass event, Emitter<PasswordsState> emit) async {}
+
+  FutureOr<void> _deletePass(
+      DeletePass event, Emitter<PasswordsState> emit) async {}
+
+  FutureOr<void> _getAllPass(
+      GetAllPass event, Emitter<PasswordsState> emit) async {}
 }
