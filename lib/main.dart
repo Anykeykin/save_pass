c00234094e9b0110c7d80431927b8779a33cce05
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-09 19:05:19 +0300

    add supabase on userRepositoryimpl

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 import 'package:save_pass/save_pass_api/local_api/repository_impl/local_repository_impl.dart';
 import 'package:save_pass/save_pass_api/remote_api/datasource/initialize.dart';
+import 'package:save_pass/save_pass_api/remote_api/repository/user_repository.dart';
+import 'package:save_pass/save_pass_api/remote_api/repository_impl/user_repository_impl.dart';
 import 'package:save_pass/save_pass_ui/first_screen.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -11,6 +13,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Database db = await LocalRepositoryImpl().openSqlDatabase();
   initialize();
-  final supabase = Supabase.instance.client;
+  UserRepositoryImpl.supabase = Supabase.instance.client;
+  UserRepository userRepository = UserRepositoryImpl();
+  
   runApp(const MyApp());
 }
