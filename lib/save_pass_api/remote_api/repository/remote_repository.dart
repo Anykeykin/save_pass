1b09c0e7ae1cbe90b0f6880f2fc1d183158f363a
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-09 22:23:22 +0300

    rmv unused signature

--- lib/save_pass_api/remote_api/repository/remote_repository.dart
+++ lib/save_pass_api/remote_api/repository/remote_repository.dart
@@ -1,23 +1,17 @@
 import 'package:save_pass/save_pass_api/models/pass_model.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract class RemoteRepository {
   Future<bool> savePass(
-    SupabaseClient supabase,
     PassModel passModel,
   );
 
-  Future<List<PassModel>> getAllPass(
-    SupabaseClient supabase,
-  );
+  Future<List<PassModel>> getAllPass();
 
   Future<bool> editPass(
-    SupabaseClient supabase,
     PassModel passModel,
   );
 
   Future<bool> deletePass(
-    SupabaseClient supabase,
     int passwordId,
   );
 }
