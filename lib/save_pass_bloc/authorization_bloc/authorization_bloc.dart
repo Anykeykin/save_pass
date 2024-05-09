07fbf80ffaefff9874699b50b16f01cfeb38f5c9
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-09 19:08:23 +0300

    rewrite authorization bloc

--- lib/save_pass_bloc/authorization_bloc/authorization_bloc.dart
+++ lib/save_pass_bloc/authorization_bloc/authorization_bloc.dart
@@ -2,20 +2,40 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:save_pass/save_pass_api/remote_api/repository/user_repository.dart';
 
 part 'authorization_event.dart';
 part 'authorization_state.dart';
 
 class AuthorizationBloc extends Bloc<AuthorizationEvent, AuthorizationState> {
-  AuthorizationBloc() : super(AuthorizationState()) {
+  final UserRepository userRepository;
+  AuthorizationBloc(this.userRepository) : super(const AuthorizationState()) {
     on<Login>(_login);
     on<Logout>(_logout);
     on<Register>(_register);
   }
 
-  FutureOr<void> _login(Login event, Emitter<AuthorizationState> emit) {}
+  FutureOr<void> _login(Login event, Emitter<AuthorizationState> emit) async {
+    final data = await userRepository.login(event.email, event.password);
+    if (data != null) {
+      emit(state.copyWith(AuthorizationStatus.access));
+    } else {
+      emit(state.copyWith(AuthorizationStatus.error));
+    }
+  }
 
-  FutureOr<void> _logout(Logout event, Emitter<AuthorizationState> emit) {}
+  FutureOr<void> _logout(Logout event, Emitter<AuthorizationState> emit) async {
+    await userRepository.logout();
+    emit(state.copyWith(AuthorizationStatus.denied));
+  }
 
-  FutureOr<void> _register(Register event, Emitter<AuthorizationState> emit) {}
+  FutureOr<void> _register(
+      Register event, Emitter<AuthorizationState> emit) async {
+    final data = await userRepository.register(event.email, event.password);
+    if (data != null) {
+      emit(state.copyWith(AuthorizationStatus.access));
+    } else {
+      emit(state.copyWith(AuthorizationStatus.error));
+    }
+  }
 }
