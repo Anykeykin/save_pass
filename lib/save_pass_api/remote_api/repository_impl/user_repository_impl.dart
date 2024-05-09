5bc0c1c6a1c4fd7956fcfccef75b217f65801772
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-09 22:23:51 +0300

    change methods to supabase service

--- lib/save_pass_api/remote_api/repository_impl/user_repository_impl.dart
+++ lib/save_pass_api/remote_api/repository_impl/user_repository_impl.dart
@@ -1,19 +1,14 @@
+import 'package:save_pass/save_pass_api/remote_api/datasource/supabase_services.dart';
 import 'package:save_pass/save_pass_api/remote_api/repository/user_repository.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class UserRepositoryImpl implements UserRepository {
-  static late SupabaseClient supabase;
-
   @override
   Future<User?> login(
     String email,
     String password,
   ) async {
-    final authorization = await supabase.auth.signInWithPassword(
-      email: email,
-      password: password,
-    );
-    return authorization.user;
+    return await SupabaseAppService.login(email, password);
   }
 
   @override
@@ -21,15 +16,11 @@ class UserRepositoryImpl implements UserRepository {
     String email,
     String password,
   ) async {
-    final authorization = await supabase.auth.signUp(
-      email: email,
-      password: password,
-    );
-    return authorization.user;
+    return await SupabaseAppService.register(email, password);
   }
 
   @override
   Future<void> logout() async {
-    await supabase.auth.signOut();
+    await SupabaseAppService.logout();
   }
 }
