9fe0079cef9b0998b2a59932e005182eb3370d41
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-10 01:07:15 +0300

    Update passwords_event.dart

--- lib/save_pass_bloc/passwords_bloc/passwords_event.dart
+++ lib/save_pass_bloc/passwords_bloc/passwords_event.dart
@@ -1,3 +1,41 @@
 part of 'passwords_bloc.dart';
 
-sealed class PasswordsEvent {}
+abstract class PasswordsEvent extends Equatable {
+  const PasswordsEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class SavePass extends PasswordsEvent {
+  final String passwordName;
+  final String password;
+
+  const SavePass({required this.passwordName, required this.password});
+
+  @override
+  List<Object> get props => [passwordName, password];
+}
+
+class EditPass extends PasswordsEvent {
+  final int passwordId;
+  final String password;
+
+  const EditPass({required this.passwordId, required this.password});
+
+  @override
+  List<Object> get props => [passwordId, password];
+}
+
+class DeletePass extends PasswordsEvent {
+  final int passwordId;
+
+  const DeletePass({required this.passwordId});
+
+  @override
+  List<Object> get props => [passwordId];
+}
+
+class GetAllPass extends PasswordsEvent {
+  const GetAllPass();
+}
