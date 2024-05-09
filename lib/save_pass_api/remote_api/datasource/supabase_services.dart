d2c732417f87f4258c5e52f6e02efc018740eadd
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-09 22:22:57 +0300

    ref

--- lib/save_pass_api/remote_api/datasource/supabase_services.dart
+++ lib/save_pass_api/remote_api/datasource/supabase_services.dart
@@ -4,14 +4,16 @@ import 'package:save_pass/save_pass_api/models/pass_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseAppService {
-  static deletePass(SupabaseClient supabase, int passwordId) async {
+  static late SupabaseClient supabase;
+
+  static deletePass(int passwordId) async {
     dynamic test =
         await supabase.from('pass').delete().match({'password_id': passwordId});
     print(test);
     return true;
   }
 
-  static editPass(SupabaseClient supabase, PassModel passModel) async {
+  static editPass(PassModel passModel) async {
     dynamic test = await supabase
         .from('pass')
         .update(passModel.toMap())
@@ -20,16 +22,42 @@ class SupabaseAppService {
     return true;
   }
 
-  static getAllPass(SupabaseClient supabase) async {
+  static getAllPass() async {
     final data = await supabase.from('users').select();
     return [
       for (final passMap in data) PassModel.fromMap(passMap),
     ];
   }
 
-  static savePass(SupabaseClient supabase, PassModel passModel) async {
+  static savePass(PassModel passModel) async {
     dynamic test = await supabase.from('cities').insert(passModel.toMap());
     print(test);
     return true;
+  }
+
+  static logout() async {
+    await supabase.auth.signOut();
+  }
+
+  static Future<User?> register(
+    String email,
+    String password,
+  ) async {
+    final authorization = await supabase.auth.signUp(
+      email: email,
+      password: password,
+    );
+    return authorization.user;
+  }
+
+  static Future<User?> login(
+    String email,
+    String password,
+  ) async {
+    final authorization = await supabase.auth.signInWithPassword(
+      email: email,
+      password: password,
+    );
+    return authorization.user;
   }
 }
