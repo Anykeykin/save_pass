596daca89f21792a8ca8d9df94b95e30ac097ae5
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-10 23:53:29 +0300

    rmv signature

--- lib/save_pass_api/local_api/repository_impl/local_repository_impl.dart
+++ lib/save_pass_api/local_api/repository_impl/local_repository_impl.dart
@@ -1,31 +1,25 @@
 import 'package:save_pass/save_pass_api/local_api/datasource/sql_local_service.dart';
 import 'package:save_pass/save_pass_api/local_api/repository/local_repository.dart';
 import 'package:save_pass/save_pass_api/models/pass_model.dart';
-import 'package:sqflite/sqflite.dart';
 
 class LocalRepositoryImpl implements LocalRepository {
   @override
-  Future<bool> deletePass(Future<Database> database, int passwordId) async {
-    return await SqlLocalService.deletePass(database, passwordId);
+  Future<bool> deletePass(int passwordId) async {
+    return await SqlLocalService.deletePass(passwordId);
   }
 
   @override
-  Future<bool> editPass(Future<Database> database, PassModel passModel) async {
-    return await SqlLocalService.editPass(database, passModel);
+  Future<bool> editPass(PassModel passModel) async {
+    return await SqlLocalService.editPass(passModel);
   }
 
   @override
-  Future<List<PassModel>> getAllPass(Future<Database> database) async {
-    return await SqlLocalService.getAllPass(database);
+  Future<List<PassModel>> getAllPass() async {
+    return await SqlLocalService.getAllPass();
   }
 
   @override
-  Future<bool> savePass(Future<Database> database, PassModel passModel)async {
-    return await SqlLocalService.savePass(database,passModel);
-  }
-
-  @override
-  openSqlDatabase() {
-    return SqlLocalService.openSqlDatabase();
+  Future<bool> savePass(PassModel passModel) async {
+    return await SqlLocalService.savePass(passModel);
   }
 }
