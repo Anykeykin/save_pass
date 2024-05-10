45f2f959e429f07af861f5b067ed1e345e3ec3be
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-05-10 23:50:38 +0300

    rmv db signature on abstract repository

--- lib/save_pass_api/local_api/repository/local_repository.dart
+++ lib/save_pass_api/local_api/repository/local_repository.dart
@@ -1,10 +1,8 @@
 import 'package:save_pass/save_pass_api/models/pass_model.dart';
-import 'package:sqflite/sqflite.dart';
 
 abstract class LocalRepository {
-  Future<bool> savePass(Future<Database> database, PassModel passModel);
-  Future<List<PassModel>> getAllPass(Future<Database> database);
-  Future<bool> editPass(Future<Database> database, PassModel passModel);
-  Future<bool> deletePass(Future<Database> database, int passwordId);
-  openSqlDatabase();
+  Future<bool> savePass(PassModel passModel);
+  Future<List<PassModel>> getAllPass();
+  Future<bool> editPass(PassModel passModel);
+  Future<bool> deletePass(int passwordId);
 }
