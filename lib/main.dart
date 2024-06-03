b7dd3ccc3180c729efc5bc713a1ad7f942c467f5
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-06-04 00:16:45 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ import 'package:save_pass/save_pass_api/remote_api/repository/user_repository.da
 import 'package:save_pass/save_pass_api/remote_api/repository_impl/remote_repository_impl.dart';
 import 'package:save_pass/save_pass_api/remote_api/repository_impl/user_repository_impl.dart';
 import 'package:save_pass/save_pass_bloc/authorization_bloc/authorization_bloc.dart';
+import 'package:save_pass/save_pass_bloc/passwords_bloc/passwords_bloc.dart';
+import 'package:save_pass/save_pass_ui/router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
@@ -27,6 +29,8 @@ void main() async {
     MyApp(
       userRepository: userRepository,
       localUserRepository: localUserRepository,
+      localRepository: localRepository,
+      remoteRepository: remoteRepository,
     ),
   );
 }
@@ -34,14 +38,23 @@ void main() async {
 class MyApp extends StatelessWidget {
   final UserRepository userRepository;
   final LocalUserRepository localUserRepository;
+  final LocalRepository localRepository;
+  final RemoteRepository remoteRepository;
+
   const MyApp(
       {super.key,
       required this.userRepository,
-      required this.localUserRepository});
+      required this.localUserRepository,
+      required this.localRepository,
+      required this.remoteRepository});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.dark,
+      initialRoute: ScreenPaths.start,
+      onGenerateRoute: SavePassRouter.routes,
+      navigatorKey: NavKey.navKey,
       title: 'SavePass',
       theme: ThemeData(
         scaffoldBackgroundColor: const Color(0xFF2C2C2C),
@@ -53,14 +66,35 @@ class MyApp extends StatelessWidget {
           bodyLarge: TextStyle(fontSize: 14.0, fontWeight: FontWeight.normal),
         ),
       ),
-      home: BlocProvider(
-        create: (context) =>
-            AuthorizationBloc(userRepository, localUserRepository)
-              ..add(AutoLogin()),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) =>
+                AuthorizationBloc(userRepository, localUserRepository)
+                  ..add(AutoLogin()),
+          ),
+          BlocProvider(
+            create: (context) => PasswordsBloc(remoteRepository,localRepository),
+          ),
+        ],
         child: BlocListener<AuthorizationBloc, AuthorizationState>(
           listener: (context, state) {
-            if (state.registrationStatus == AuthorizationStatus.denied) {}
-            if (state.registrationStatus == AuthorizationStatus.access) {}
+            if (state.registrationStatus == AuthorizationStatus.denied) {
+              Navigator.of(context).pushNamed(
+                ScreenPaths.loginScreen,
+                arguments: {
+                  'auth_bloc': context.read<AuthorizationBloc>(),
+                },
+              );
+            }
+            if (state.registrationStatus == AuthorizationStatus.access) {
+              Navigator.of(context).pushNamed(
+                ScreenPaths.passListScreen,
+                arguments: {
+                  'passwords_bloc': context.read<PasswordsBloc>(),
+                },
+              );
+            }
           },
           child: const LoadingScreen(),
         ),
@@ -85,4 +119,8 @@ class LoadingScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+mixin NavKey {
+  static final navKey = GlobalKey<NavigatorState>();
 }
