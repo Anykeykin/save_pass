e1bf07ace9f887ea9fa22874b896b1c0e28926d1
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-06-10 23:07:22 +0300

    add level abstract methods

--- lib/save_pass_api/local_api/repository/local_repository.dart
+++ lib/save_pass_api/local_api/repository/local_repository.dart
@@ -1,8 +1,11 @@
 import 'package:save_pass/save_pass_api/models/pass_model.dart';
+import 'package:save_pass/save_pass_api/models/security_level.dart';
 
 abstract class LocalRepository {
   Future<bool> savePass(PassModel passModel);
   Future<List<PassModel>> getAllPass();
   Future<bool> editPass(PassModel passModel);
   Future<bool> deletePass(int passwordId);
+  Future<bool> saveSecurityLevel(SecurityLevel securityLevel);
+  Future<SecurityLevel> getSecurityLevel();
 }
