f6e91154b89138a4fa05d007c8353bfb241b3171
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-06-10 23:05:57 +0300

    add security level on passwords state

--- lib/save_pass_bloc/passwords_bloc/passwords_state.dart
+++ lib/save_pass_bloc/passwords_bloc/passwords_state.dart
@@ -6,23 +6,33 @@ class PasswordsState extends Equatable {
   final LoadStatus loadStatus;
   final List<PassModel> passModel;
   final int passwordId;
+  final String securityLevel;
   @override
-  List<Object?> get props => [loadStatus, passModel, passwordId];
+  List<Object?> get props => [
+        loadStatus,
+        passModel,
+        passwordId,
+        securityLevel,
+      ];
 
   const PasswordsState({
     this.loadStatus = LoadStatus.loading,
     this.passModel = const [],
     this.passwordId = 0,
+    this.securityLevel = 'base',
   });
 
   PasswordsState copyWith({
     LoadStatus? loadStatus,
     List<PassModel>? passModel,
     int? passwordId,
+    String? securityLevel,
   }) {
     return PasswordsState(
-        passModel: passModel ?? this.passModel,
-        loadStatus: loadStatus ?? this.loadStatus,
-        passwordId: passwordId ?? this.passwordId);
+      passModel: passModel ?? this.passModel,
+      loadStatus: loadStatus ?? this.loadStatus,
+      passwordId: passwordId ?? this.passwordId,
+      securityLevel: securityLevel ?? this.securityLevel,
+    );
   }
 }
