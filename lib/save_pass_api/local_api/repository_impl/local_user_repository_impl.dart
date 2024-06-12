3c5246064ee4baa98a0a5677768b42b63af06360
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-06-13 02:34:06 +0300

    rmv

--- lib/save_pass_api/local_api/repository_impl/local_user_repository_impl.dart
+++ lib/save_pass_api/local_api/repository_impl/local_user_repository_impl.dart
@@ -4,11 +4,6 @@ import 'package:save_pass/save_pass_api/models/app_user.dart';
 
 class LocalUserRepositoryImpl implements LocalUserRepository {
   @override
-  Future<AppUser?> loadAuthData() async {
-    return await SqlLocalService.loadAuthData();
-  }
-
-  @override
   Future<bool> saveAuthData(AppUser appUser) async {
     return await SqlLocalService.saveAuthData(appUser);
   }
