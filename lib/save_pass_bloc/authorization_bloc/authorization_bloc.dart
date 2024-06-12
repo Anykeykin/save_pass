78e9c4d8d3a487deb6ddf020bcb683bac6f2d7d7
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-06-13 02:34:17 +0300

    add placeholder

--- lib/save_pass_bloc/authorization_bloc/authorization_bloc.dart
+++ lib/save_pass_bloc/authorization_bloc/authorization_bloc.dart
@@ -46,12 +46,12 @@ class AuthorizationBloc extends Bloc<AuthorizationEvent, AuthorizationState> {
 
   FutureOr<void> _autoLogin(
       AutoLogin event, Emitter<AuthorizationState> emit) async {
-    AppUser? appUser = await localUserRepository.loadAuthData();
-    if (appUser == null) {
-      emit(state.copyWith(AuthorizationStatus.denied));
-    }
-    if (appUser != null) {
-      add(Login(email: appUser.email, password: appUser.password));
-    }
+    emit(state.copyWith(AuthorizationStatus.denied));
+    // if (appUser == null) {
+    //   emit(state.copyWith(AuthorizationStatus.denied));
+    // }
+    // if (appUser != null) {
+    //   add(Login(email: appUser.email, password: appUser.password));
+    // }
   }
 }
