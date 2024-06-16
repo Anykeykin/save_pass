910921ddf7a5ddc6d081014230e5d48a4ee9bdbf
Anykeykin <69866986+Anykeykin@users.noreply.github.com>
2024-06-16 22:10:27 +0300

    change auth state fields

--- lib/save_pass_bloc/authorization_bloc/authorization_state.dart
+++ lib/save_pass_bloc/authorization_bloc/authorization_state.dart
@@ -1,22 +1,33 @@
 part of 'authorization_bloc.dart';
 
-enum AuthorizationStatus { access, denied, error }
+enum OpenStatus { loading, access, denied, create, error }
 
 class AuthorizationState extends Equatable {
-  final AuthorizationStatus registrationStatus;
-
+  final OpenStatus openStatus;
+  final String levelKey;
+  final String firstKey;
+  final String secondKey;
   @override
-  List<Object?> get props => [registrationStatus];
+  List<Object?> get props => [openStatus, levelKey, firstKey, secondKey];
 
   const AuthorizationState({
-    this.registrationStatus = AuthorizationStatus.denied,
+    this.openStatus = OpenStatus.loading,
+    this.levelKey = '',
+    this.firstKey = '',
+    this.secondKey = '',
   });
 
-  AuthorizationState copyWith(
-    AuthorizationStatus? registrationStatus,
-  ) {
+  AuthorizationState copyWith({
+    OpenStatus? openStatus,
+    String? levelKey,
+    String? firstKey,
+    String? secondKey,
+  }) {
     return AuthorizationState(
-      registrationStatus: registrationStatus ?? this.registrationStatus,
+      openStatus: openStatus ?? this.openStatus,
+      levelKey: levelKey ?? this.levelKey,
+      firstKey: firstKey ?? this.firstKey,
+      secondKey: secondKey ?? this.secondKey,
     );
   }
 }
