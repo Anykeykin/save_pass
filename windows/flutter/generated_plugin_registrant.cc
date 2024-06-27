5baff803be0412aa3ed6b31c853caab6cf62fe09
Ainur <69866986+Anykeykin@users.noreply.github.com>
2024-06-27 02:32:08 -0700

    upd platforms

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <app_links/app_links_plugin_c_api.h>
+#include <encrypt_decrypt_plus/encrypt_decrypt_plus_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AppLinksPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AppLinksPluginCApi"));
+  EncryptDecryptPlusPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("EncryptDecryptPlusPluginCApi"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
+  encrypt_decrypt_plus
   url_launcher_windows
 )
 
