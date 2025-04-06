1654e9f91b1820728890a40673284ad380010840
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-04-06 22:33:03 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:domain/local_repository/local_repository.dart';
-import 'package:save_pass/save_pass_api/local_api/repository_impl/local_repository_impl.dart';
+import 'package:local_data/repository_impl/local_repository_impl.dart';
 import 'package:save_pass/save_pass_bloc/authorization_bloc/authorization_bloc.dart';
 import 'package:save_pass/save_pass_bloc/passwords_bloc/passwords_bloc.dart';
 import 'package:save_pass/save_pass_ui/router.dart';
