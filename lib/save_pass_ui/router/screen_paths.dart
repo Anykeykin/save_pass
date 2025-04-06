85677bc1116f38b54ef919cf6bc4cfd061df874c
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-04-06 23:16:50 +0300

    add screen path file

---
+++ lib/save_pass_ui/router/screen_paths.dart
@@ -0,0 +1,8 @@
+class ScreenPaths {
+  static const String start = '/';
+  static const String passListScreen = 'passListScreen';
+  static const String registrationScreen = 'registrationScreen';
+  static const String loginScreen = 'loginScreen';
+  static const String createPassListScreen = 'createPassListScreen';
+  static const String editPassListScreen = 'editPassListScreen';
+}
