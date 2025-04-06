8cfbdf9581d316f0a75873f32210d003b6564c54
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-04-06 22:19:58 +0300

    change local repository path on api

--- lib/save_pass_api/local_api/repository_impl/local_repository_impl.dart
+++ lib/save_pass_api/local_api/repository_impl/local_repository_impl.dart
@@ -1,7 +1,7 @@
 import 'package:domain/models/pass_model.dart';
 import 'package:domain/models/security_level.dart';
 import 'package:save_pass/save_pass_api/local_api/datasource/sql_local_service.dart';
-import 'package:save_pass/save_pass_api/local_api/repository/local_repository.dart';
+import 'package:domain/local_repository/local_repository.dart';
 
 class LocalRepositoryImpl implements LocalRepository {
   @override
