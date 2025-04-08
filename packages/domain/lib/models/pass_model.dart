ce9ca1ca179aa7ac0e3d1b448ead314d8bfeda68
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-04-08 14:24:19 +0300

    upd pass model

--- packages/domain/lib/models/pass_model.dart
+++ packages/domain/lib/models/pass_model.dart
@@ -8,6 +8,19 @@ class PassModel {
     required this.passwordId,
   });
 
+  // Метод copyWith
+  PassModel copyWith({
+    String? passwordName,
+    String? password,
+    int? passwordId,
+  }) {
+    return PassModel(
+      passwordName: passwordName ?? this.passwordName,
+      password: password ?? this.password,
+      passwordId: passwordId ?? this.passwordId,
+    );
+  }
+
   Map<String, Object?> toMap() {
     return {
       'password': password,
@@ -18,9 +31,14 @@ class PassModel {
 
   static PassModel fromMap(Map<String, dynamic> map) {
     return PassModel(
-      passwordName: map['password_name'],
-      password: map['password'],
-      passwordId: map['password_id'],
+      passwordName: map['password_name'] as String,
+      password: map['password'] as String,
+      passwordId: map['password_id'] as int,
     );
+  }
+
+  @override
+  String toString() {
+    return 'PassModel(passwordName: $passwordName, password: [hidden], passwordId: $passwordId)';
   }
 }
