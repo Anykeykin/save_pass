ca13aff997bdda96cd9a2299d18c39f729131eb9
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-04-18 00:12:07 +0300

    change methods static for sql local service instance

--- packages/local_data/lib/repository_impl/local_repository_impl.dart
+++ packages/local_data/lib/repository_impl/local_repository_impl.dart
@@ -4,44 +4,48 @@ import 'package:local_data/datasource/sql_local_service.dart';
 import 'package:domain/local_repository/local_repository.dart';
 
 class LocalRepositoryImpl implements LocalRepository {
+  final SqlLocalService _sqlLocalService;
+
+  LocalRepositoryImpl(this._sqlLocalService);
+
   @override
   Future<bool> deletePass(int passwordId) async {
-    return await SqlLocalService.deletePass(passwordId);
+    return await _sqlLocalService.deletePass(passwordId);
   }
 
   @override
   Future<bool> editPass(PassModel passModel) async {
-    return await SqlLocalService.editPass(passModel);
+    return await _sqlLocalService.editPass(passModel);
   }
 
   @override
   Future<List<PassModel>> getAllPass() async {
-    return await SqlLocalService.getAllPass();
+    return await _sqlLocalService.getAllPass();
   }
 
   @override
   Future<bool> savePass(PassModel passModel) async {
-    return await SqlLocalService.savePass(passModel);
+    return await _sqlLocalService.savePass(passModel);
   }
 
   @override
   Future<bool> saveSecurityKey(SecurityKey securityKey) async {
-    return await SqlLocalService.saveKey(securityKey);
+    return await _sqlLocalService.saveKey(securityKey);
   }
 
   @override
   Future<List<SecurityKey>> getKeys() async {
-    return await SqlLocalService.getKeys();
+    return await _sqlLocalService.getKeys();
   }
 
   @override
   Future<bool> saveLevel(String level) async {
-    return await SqlLocalService.saveLevel(level);
+    return await _sqlLocalService.saveLevel(level);
   }
 
   @override
   Future<String> getLevel() async {
-    return await SqlLocalService.getLevel();
+    return await _sqlLocalService.getLevel();
   }
 
   @override
