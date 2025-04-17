e9e1d79275e7474c472d83a65534296b8b5bf211
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-04-18 00:16:47 +0300

    change init local repository in main

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,16 @@
 import 'package:flutter/material.dart';
 import 'package:domain/local_repository/local_repository.dart';
 import 'package:local_data/repository_impl/local_repository_impl.dart';
+import 'package:local_data/datasource/sql_local_service.dart';
 import 'package:save_pass/save_pass_ui/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  LocalRepository localRepository = LocalRepositoryImpl();
-  localRepository.initDatabase();
+
+  LocalRepository localRepository = LocalRepositoryImpl(
+    SqlLocalService(),
+  );
+
+  await localRepository.initDatabase();
   runApp(MyApp(localRepository: localRepository));
 }
-
